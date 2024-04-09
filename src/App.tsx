97133860391525e732400b1c0f22a1689d94535d
Usuario <usuario@MacBook-Pro-de-Usuario-2.local>
2024-04-08 22:37:03 -0500

    Finalizado repaso interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Title from './organisms/title';
+import Antiguo from './organisms/classes-antiguo';
+import CIButton from './organisms/ci-button';
 
 function App() {
   return (
     <div>
       <Title/>
+      <Antiguo/>
+      <CIButton onClick={()=>{}} variant="contained" color="sucess">Click me</CIButton>
     </div>
   );
 }
